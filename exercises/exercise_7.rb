3e073efbff3792c30c344c30defa85decd403b03
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-26 23:05:39 -0400

    Update models, add employees

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,11 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# 1. Add data into employees
+@store1 = Store.find_by(id: 1)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Willy", last_name: "Wonka", hourly_rate: 60)
+@store1.employees.create(first_name: "Milly", last_name: "Monka", hourly_rate: 60)
+@store2 = Store.find_by(id: 2)
+@store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 100)
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 80)

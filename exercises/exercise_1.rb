e9038dc67a702f9737f3d028058d67e9072743bf
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-26 22:43:28 -0400

    Add comments to ex1, complete Exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,9 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 # Exercise 1: Create 3 stores
+
+# 1. Create the 3 stores in the database
 burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
-# Print the number of stores
+# 2. Print the number of stores in the database
 puts Store.count
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Exercise 2: Update the first store
+
+# 1. Load the first store
+@store1 = Store.find_by(id: 1)
+
+# 2. Load the second store
+@store2 = Store.find_by(id: 2)
+
+# 3. Update the first store instance in the database
+@store1.name = "Wendy's"
+@store1.save

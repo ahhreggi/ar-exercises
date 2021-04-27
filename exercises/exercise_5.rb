1a889150c08d8333b4489dcc5917a76d4323f41f
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-26 22:59:03 -0400

    Complete Exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,13 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# Exercise 5: Calculations
+
+# 1. Output the total revenue for the entire company
+puts Store.sum(:annual_revenue)
+
+# 2. Output the average annual revenue for all stores
+puts Store.average(:annual_revenue)
+
+# 3. Output the number of stores that are generating $1M or more in annual sales
+puts Store.where("annual_revenue >= 1000000").size
